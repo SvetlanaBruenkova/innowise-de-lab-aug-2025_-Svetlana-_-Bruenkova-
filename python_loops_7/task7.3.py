49b4c7c2f0f686dde2e96703500d07322d57e6a0
Светлана Бруенкова <svetos_br@mail.ru>
2025-09-02 18:10:16 +0300

    Обновлен homework7

--- python_loops_7/task7.3.py
+++ python_loops_7/task7.3.py
@@ -1,11 +1,9 @@
 #Задача 3: Угадай число
-import random
-
-n = random.randint(1, 10)
+secret_number = 7
 
 while True:
     a = int(input("Угадай число от 1 до 10: "))
-    if a == n:
+    if a == secret_number:
         print("Поздравляю! Вы угадали число!")
         break
     else:
